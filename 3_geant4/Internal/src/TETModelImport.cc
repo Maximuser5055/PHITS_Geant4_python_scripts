3aaeb0d452b6552f5dc3269260906f23d09a65d8
Clarence Albert S. Feliciano <csfeliciano2@up.edu.ph>
2026-08-19 15:00:25 +0800

    Updated August 19

--- 3_geant4/Internal/src/TETModelImport.cc
+++ 3_geant4/Internal/src/TETModelImport.cc
@@ -273,7 +273,6 @@ void TETModelImport::PrintMaterialInfomation()
 
 	// Write to a csv file
 	namespace fs = std::filesystem;
-	fs::create_directories("../../../4_results/geant4");
 	std::ofstream summaryFile("../../../4_results/b_geant4/0_organ_summary.csv");
 	summaryFile << "OrganID,NumTet,Volume_cm3,Density_gcm3,Mass_g,Organ\n";
 
